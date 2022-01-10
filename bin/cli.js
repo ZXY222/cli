bae7ab5772dbe16d9bd33afe504a14ebe0ca59bc
zxy222 <313700607@qq.com>
2022-01-10 17:40:42 +0800

    #FEAT 初始化

---
+++ bin/cli.js
@@ -0,0 +1,62 @@
+#! /usr/bin/env node
+
+const program = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
+
+program
+    // 定义命令和参数
+    .command('create <app-name>')
+    .description('create a new project')
+    // -f or --force 为强制创建，如果创建的目录存在则直接覆盖,如果有重复options中force属性会为true
+    .option('-f, --force', chalk.hex('#049CDB').bold('是否覆盖存在目录'))
+    .action((name, options) => {
+        require('../lib/index')(name, options)
+    })
+
+
+// 配置config
+program
+    .command('config [value]')
+    .description('inspect and modify the config')
+    .option('-g, --get <path>', 'get value from option')
+    .option('-s, --set <path> <value>')
+    .option('-d, --delete <path>', 'delete option from config')
+    .action((value, options) => {
+        console.log(value, options)
+    })
+
+// 配置 ui 命令
+program
+    .command('ui')
+    .description('start add open roc-cli ui')
+    .option('-p, --port <port>', 'Port used for the UI Server')
+    .action((option) => {
+        console.log(option)
+    })
+
+
+program
+    // 监听 --help 执行
+    .on('--help', () => {
+        // 新增说明信息
+        // 使用 figlet 绘制 Logo
+        console.log('\r\n' + figlet.textSync('xy', {
+            font: 'Ghost',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 80,
+            whitespaceBreak: true
+        }));
+        // 新增说明信息
+        console.log(`\r\nRun ${chalk.cyan(`roc <command> --help`)} show details\r\n`)
+    })
+
+
+program
+    // 配置版本号信息
+    .version(`v${require('../package.json').version}`)
+    .usage('<command> [option]')
+
+// 解析用户执行命令传入参数
+program.parse(process.argv);
